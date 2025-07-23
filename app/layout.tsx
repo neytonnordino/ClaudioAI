276ab50dbdaefae37c26fd3aa6b1300b7761e2d3
Neyton <193637348+neytonnordino@users.noreply.github.com>
2025-07-23 21:10:41 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <UserProvider>
         <Providers>
-          <body className="h-screen">
+          <body className="h-dvh">
             <ClientLayout>{children}</ClientLayout>
             <Toaster
               position="top-right"
