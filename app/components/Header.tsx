0829b45591dd2d80c947381549480c433e4ec8f0
Neyton <193637348+neytonnordino@users.noreply.github.com>
2025-07-24 15:14:48 +0200

    commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,7 +14,7 @@ type SideBarProps = {
 };
 
 function Header({ isOpen, toggleSideBar }: SideBarProps) {
-const [isShown, setIsShown] = useState(false);
+  const [isShown, setIsShown] = useState(false);
 
   const { data: session } = useSession();
 
@@ -52,28 +52,34 @@ const [isShown, setIsShown] = useState(false);
             />
           </svg>
         </button>
-        <button className="flex gap-2 items-center cursor-pointer hover:bg-white/10 group p-2 rounded-md transition-all">
-          <h2 className="text-xl md:text-2xl text-white  transition ">
-            Cláudio
-          </h2>
-          <svg
-            width="20"
-            height="21"
-            viewBox="0 0 20 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5"
-          >
-            <path
-              d="M10.5575 14.6925L4.3075 8.44254C4.24944 8.38447 4.20338 8.31553 4.17195 8.23966C4.14052 8.16379 4.12435 8.08247 4.12435 8.00035C4.12435 7.91823 4.14052 7.83691 4.17195 7.76104C4.20338 7.68517 4.24944 7.61623 4.3075 7.55816C4.36557 7.50009 4.43451 7.45403 4.51038 7.4226C4.58625 7.39117 4.66757 7.375 4.7497 7.375C4.83182 7.375 4.91315 7.39117 4.98902 7.4226C5.06489 7.45403 5.13383 7.50009 5.1919 7.55816L11.0006 13.3668L16.8083 7.55816C16.9256 7.44088 17.0846 7.375 17.2505 7.375C17.4163 7.375 17.5754 7.44088 17.6927 7.55816C17.81 7.67544 17.8759 7.8345 17.8759 8.00035C17.8759 8.1662 17.81 8.32526 17.6927 8.44254L11.4427 14.6925C11.3846 14.7506 11.3157 14.7967 11.2398 14.8282C11.164 14.8597 11.0826 14.8758 11.0005 14.8758C10.9183 14.8758 10.837 14.8597 10.7611 14.8282C10.6853 14.7967 10.6163 14.7506 10.5575 14.6925Z"
-              fill="#ffffffa4"
-            />
-          </svg>
+        <button
+          className="flex  items-center cursor-pointer hover:bg-white/10 group p-2 rounded-md transition-all duration-200 relative"
+          onClick={() => setIsShown((prev) => !prev)}
+        >
+          <div className="flex items-center gap-2">
+            <h2 className="text-xl md:text-2xl text-white  transition-all ">
+              Cláudio
+            </h2>
+            <svg
+              width="20"
+              height="21"
+              viewBox="0 0 20 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5"
+            >
+              <path
+                d="M10.5575 14.6925L4.3075 8.44254C4.24944 8.38447 4.20338 8.31553 4.17195 8.23966C4.14052 8.16379 4.12435 8.08247 4.12435 8.00035C4.12435 7.91823 4.14052 7.83691 4.17195 7.76104C4.20338 7.68517 4.24944 7.61623 4.3075 7.55816C4.36557 7.50009 4.43451 7.45403 4.51038 7.4226C4.58625 7.39117 4.66757 7.375 4.7497 7.375C4.83182 7.375 4.91315 7.39117 4.98902 7.4226C5.06489 7.45403 5.13383 7.50009 5.1919 7.55816L11.0006 13.3668L16.8083 7.55816C16.9256 7.44088 17.0846 7.375 17.2505 7.375C17.4163 7.375 17.5754 7.44088 17.6927 7.55816C17.81 7.67544 17.8759 7.8345 17.8759 8.00035C17.8759 8.1662 17.81 8.32526 17.6927 8.44254L11.4427 14.6925C11.3846 14.7506 11.3157 14.7967 11.2398 14.8282C11.164 14.8597 11.0826 14.8758 11.0005 14.8758C10.9183 14.8758 10.837 14.8597 10.7611 14.8282C10.6853 14.7967 10.6163 14.7506 10.5575 14.6925Z"
+                fill="#ffffffa4"
+              />
+            </svg>
+          </div>
+
           <div
-            onClick={() => setIsShown((prev) => !prev)}
-            className={`hidden ${isShown ? "hidden" : "inline-flex"}`}
+            className={`hidden transition-all delay-200 absolute -bottom-8 left-5 ${
+              isShown ? "hidden" : "inline-flex"
+            }`}
           >
-            <LogOut />
           </div>
         </button>
         <svg
